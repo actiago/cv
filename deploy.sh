ac4d0671d0c26f2e0555881fa42db171cb4ebef9
Gabriel Dutra <0xdutra@gmail.com>
2021-02-20 12:00:00 -0300

    Add cv

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+HOST="0xdutra.com"
+USER="freebsd"
+REMOTE_DIR="/usr/local/share/resume/"
+
+function check_commands() {
+    if ! command -v hugo &> /dev/null; then
+        echo "Please, install hugo :)"
+        exit 1
+
+    elif ! command -v rsync &> /dev/null; then
+        echo "Please, install rsync :)"
+        exit 1
+    fi
+}
+
+function main() {
+    echo "[+] Executing hugo site static generator..."
+    hugo
+
+    echo "[+] Executing deploy to ${HOST}"
+    rsync -avz --delete public/ ${USER}@${HOST}:/${REMOTE_DIR}
+
+    exit 0
+}
+
+check_commands
+main
